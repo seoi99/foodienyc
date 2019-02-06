28d7cb47314dbb528cc724860142717891a65fde
seo9204 <seo9204@gmail.com>
2019-02-06 16:37:51 -0500

    delete one business

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Business.destroy_all
-Hour.destroy_all
-Image.destroy_all
-Review.destroy_all
+Business.destroy(Business.find_by(business_name: "Anytime").id)
 
-korean = YelpApiBusiness.new()
-korean.search("korean", "new york city")
-
-japanese = YelpApiBusiness.new()
-japanese.search("japanese", "new york city")
-
-italian = YelpApiBusiness.new()
-italian.search("italian", "new york city")
-
-fastfood = YelpApiBusiness.new()
-fastfood.search("fast food", "new york city")
-
-salad = YelpApiBusiness.new()
-salad.search("salad", "new york city")
-
-cafe = YelpApiBusiness.new()
-cafe.search("cafe", "new york city")
-
-delivery = YelpApiBusiness.new()
-delivery.search("delivery", "new york city")
+# Business.destroy_all
+# Hour.destroy_all
+# Image.destroy_all
+# Review.destroy_all
+#
+# korean = YelpApiBusiness.new()
+# korean.search("korean", "new york city")
+#
+# japanese = YelpApiBusiness.new()
+# japanese.search("japanese", "new york city")
+#
+# italian = YelpApiBusiness.new()
+# italian.search("italian", "new york city")
+#
+# fastfood = YelpApiBusiness.new()
+# fastfood.search("fast food", "new york city")
+#
+# salad = YelpApiBusiness.new()
+# salad.search("salad", "new york city")
+#
+# cafe = YelpApiBusiness.new()
+# cafe.search("cafe", "new york city")
+#
+# delivery = YelpApiBusiness.new()
+# delivery.search("delivery", "new york city")
